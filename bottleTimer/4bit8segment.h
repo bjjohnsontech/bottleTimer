4d6fc49d9d693e6d5a079f4b1fe47e35c36b533f
BJ Johnson <iam@bjjohnson.tech>
2019-04-03 10:53:48 -0400

    added frame for segment library

---
+++ bottleTimer/4bit8segment.h
@@ -0,0 +1,10 @@
+#ifndef 4bit8segment_h_
+#define 4bit8segment_h_
+
+class 4bit8segment{
+  void initialize(int pin0, int pin1, int pin2, int pin3, int pin4, int pin5,
+                  int pin6, int pin7, int pin8, int pin9, int pin10, int pin11) __attribute__((always_inline)) {
+    
+    setPeriod(microseconds);
+  }
+}
